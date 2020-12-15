cc588ac1a17c679954b695fe21133bd3676ac985
Alone1t <1029170395@qq.com>
2020-12-15 21:48:10 +0800

    Thread

--- 202012131/src/lesson2/InterruptTest.java
+++ 202012131/src/lesson2/InterruptTest.java
@@ -1,5 +1,55 @@
 package lesson2;
 
 public class InterruptTest {
-    
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Thread.interrupted()返回当前线程的中断标志位，然后重置中断标志位
+                for(int i = 0; i < 10000;i++) {
+                    System.out.println(i+":"+Thread.interrupted());
+                }
+
+//                for(int i = 0; i < 10000 && !Thread.interrupted();i++) {
+//                    try {
+//                        System.out.println(i);
+//                        Thread.sleep(1000);  //模拟中断线程
+//                        //Thread.sleep(100000); //通过标志位自行实现，无法解决线程阻塞导致无法中断
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+
+//                for(int i = 0; i < 10000 && !Thread.currentThread().isInterrupted();i++) {
+//                    try {
+//                        System.out.println(i);
+//                        Thread.sleep(1000);  //模拟中断线程
+//                        //Thread.sleep(100000); //通过标志位自行实现，无法解决线程阻塞导致无法中断
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+
+//                try {
+//                    for (int i = 0; i < 10000 && !Thread.currentThread().isInterrupted(); i++) {
+//                        System.out.println(i);
+//                        Thread.sleep(1000);  //模拟中断线程
+//                        //Thread.sleep(100000); //通过标志位自行实现，无法解决线程阻塞导致无法中断
+//                    }
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+            }
+        });
+        t.start(); //线程启动 中断标志位为false
+        System.out.println("t_start");
+        //模拟t执行了5秒还没有结束，要中断、停止t线程
+        //让t线程中断掉,停止
+        Thread.sleep(5000);
+        //告诉t线程要中断了（设置t线程的中断标志位为true） 由t的代码自己决定要不要中断
+        //如果t线程处于阻塞状态，会抛出InterruptedException异常，并且重置t线程的中断标志位
+        t.interrupt();
+        System.out.println("t_stop");
+
+    }
 }
---
+++ 202012131/src/lesson2/InterruptTest2.java
@@ -0,0 +1,29 @@
+package lesson2;
+
+public class InterruptTest2 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Thread.interrupted()返回当前线程的中断标志位，然后重置中断标志位
+                for(int i = 0; i < 10;i++) {
+                    System.out.println(i+":"+Thread.interrupted());
+                }
+            }
+        });
+        t.start(); //线程启动 中断标志位为false
+        System.out.println("t_start");
+        //模拟t执行了5秒还没有结束，要中断、停止t线程
+        //让t线程中断掉,停止
+
+        //告诉t线程要中断了（设置t线程的中断标志位为true） 由t的代码自己决定要不要中断
+        //如果t线程处于阻塞状态，会抛出InterruptedException异常，并且重置t线程的中断标志位
+        t.interrupt();
+        System.out.println("t_stop");
+//        t_start  (结果)8
+//        t_stop
+//        0:false
+//        1:true
+//        2:false
+    }
+}
