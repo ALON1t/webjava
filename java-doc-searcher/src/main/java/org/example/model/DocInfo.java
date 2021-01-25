128011e85329a7aa97d6662ead7440a12657efc4
Alone1t <1029170395@qq.com>
2021-01-25 11:23:53 +0800

    search

---
+++ java-doc-searcher/src/main/java/org/example/model/DocInfo.java
@@ -0,0 +1,56 @@
+package org.example.model;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+/**
+ * 每一个本地html文件对应一个文档对象
+ */
+
+public class DocInfo {
+    private Integer id;//类似数据库里的id(主键)，识别不同的文档
+    private String title;//标题：html的文件名作为我们的标题
+    private String url; //oracle官网api文档下html的url
+    private String content;//网页正文：html（<标签>内容</标签>），内容为正文
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "DocInfo{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
